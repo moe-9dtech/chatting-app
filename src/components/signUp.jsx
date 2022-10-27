d7a82636dd299ecb288a47c39241fa82cee88579
moe-9dtech <muhammadmoaid@9dtechnologies.com>
2022-10-27 18:05:24 +0500

    login page

---
+++ src/components/signUp.jsx
@@ -0,0 +1,66 @@
+import React, {Component} from 'react';
+import './signUpStyle.css';
+
+class LoginCard extends Component { 
+    state = {
+        userName: '',
+        password: ''
+    }
+    getUser = (event) => {
+        this.setState({userName: event.target.value});
+    }
+    getPass = (event) => {
+        this.setState({password: event.target.value});
+    }
+    loginFunction = (event) => {
+         const userName = this.state.userName;
+         const password = this.state.password;
+         this.setState({userName})
+         if (userName === "rocket" && password === "123321"){
+            console.log("you are logged in!");
+         } else {
+            console.log("incorrect credentials!!");
+         }
+    }
+    render() { 
+        
+        return (
+            <React.Fragment>
+                <div className="container __cardContainer text-center">
+                    <form className='login-form' action="#">
+                        <input 
+                            className='name' 
+                            type="text" 
+                            placeholder='UserName' 
+                            onChange={this.getUser}
+                        />
+                        <input 
+                            className='pass' 
+                            type="text" 
+                            placeholder='Password' 
+                            onChange={this.getPass}
+                        />
+                        <button 
+                            className="btn login-btn" 
+                            onClick={this.loginFunction}
+                        >
+                            Login
+                        </button>
+                        <a href="#"><p>Forgot Password?</p></a>
+                        <button className='btn google-btn'>
+                            <span><i className="fa-brands fa-google"></i> </span>
+                            Login With Google
+                        </button>
+                        <a href="#"><p>OR</p></a>
+                        <button className='btn register-btn'>
+                            <span><i className="fa-solid fa-right-to-bracket"></i> </span>
+                            Register
+                        </button>
+                    </form>
+                </div>
+            </React.Fragment>
+        );
+    }
+}
+ 
+export default LoginCard;
---
+++ src/components/userProfile.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class UserProfile extends Component {
+   
+    render() { 
+        return (
+            <React.Fragment>
+
+            </React.Fragment>
+        );
+    }
+}
+ 
+export default UserProfile;
