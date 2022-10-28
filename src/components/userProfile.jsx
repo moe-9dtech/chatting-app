14d9322ff9d6ee9636aeb20b42b354ddf1d1ca72
moe-9dtech <muhammadmoaid@9dtechnologies.com>
2022-10-28 18:12:43 +0500

    separate live streaming website

--- src/components/userProfile.jsx
+++ src/components/userProfile.jsx
@@ -5,7 +5,9 @@ class UserProfile extends Component {
     render() { 
         return (
             <React.Fragment>
-
+                <div className="__left-menue">
+                    <span><i class="fa-solid fa-user"></i></span><p>General</p>
+                </div>
             </React.Fragment>
         );
     }
